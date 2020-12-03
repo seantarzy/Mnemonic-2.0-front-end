acb474ac4285e234ed693343b7865ab5cced93e8
Sean Tarzy <seantarzy@gmail.com>
2020-12-03 01:27:52 -0500

    redirect

--- src/components/EditNotes.jsx
+++ src/components/EditNotes.jsx
@@ -20,7 +20,7 @@ export default class EditNodes extends React.Component {
         .then(()=>{
             this.props.toggleEditNotes()
             // window.location.reload(false);
-            window.location.replace("https://mnemonicmaker.netlify.app/playlists");
+            window.location.replace("https://mnemonicmaker.netlify.app/");
         // this.props.setNewNote(this.state.currentNote)
         })
         .then(()=>{           
