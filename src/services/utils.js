7b5218f767046bdc72634298f45073d98767c5e1
Sean Tarzy <seantarzy@gmail.com>
2020-12-04 11:52:28 -0500

    no more incrementing snippet index in backend

--- src/services/utils.js
+++ src/services/utils.js
@@ -3,7 +3,7 @@ const local = "http://localhost:3001/";
 
 const heroku = "https://lit-everglades-15783.herokuapp.com/"
 
-const BACKEND = local
+const BACKEND = heroku
 
 export const fetchMnemonic = (phrase, current_snippet_index, artist, order, fresh_search=true) => {
   if(!artist){
